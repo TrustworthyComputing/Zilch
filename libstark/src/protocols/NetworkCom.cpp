c8b445681c3aac288a88ce18e5260bf2dd7efaee
Dimitris Mouris <jimouris@udel.edu>
2019-03-11 15:20:06 -0400

    Cleanup sockets class

--- libstark/src/protocols/NetworkCom.cpp
+++ libstark/src/protocols/NetworkCom.cpp
@@ -11,39 +11,21 @@ typedef void raw_type;       // Type used for raw data on this platform
 
 using namespace std;
 
-// SocketException Code
-
-SocketException::SocketException(const string &message, bool inclSysMsg): userMessage(message) {
-    if (inclSysMsg) {
-        userMessage.append(": ");
-        userMessage.append(strerror(errno));
-    }
-}
-
-SocketException::~SocketException(){}
-
-
-// Function to fill in address structure given an address and port
 static void fillAddr(const string &address, unsigned short port, sockaddr_in &addr) {
     memset(&addr, 0, sizeof(addr));  // Zero out address structure
     addr.sin_family = AF_INET;       // Internet address
     
     hostent *host;  // Resolve name
     if ((host = gethostbyname(address.c_str())) == NULL) {
-        // strerror() will not work for gethostbyname() and hstrerror() 
-        // is supposedly obsolete
-        // throw SocketException("Failed to resolve name (gethostbyname())");
+        perror("Failed to resolve name (gethostbyname())");
     }
     addr.sin_addr.s_addr = *((unsigned long *) host->h_addr_list[0]);
     addr.sin_port = htons(port);     // Assign port in network byte order
 }
 
-// Socket Code
-
 Socket::Socket(int type, int protocol) {
-    // Make a new socket
     if ((sockDesc = socket(PF_INET, type, protocol)) < 0) {
-        // throw SocketException("Socket creation failed (socket())", true);
+        perror("Socket creation failed (socket())");
     }
 }
 
@@ -60,7 +42,7 @@ string Socket::getLocalAddress() {
     sockaddr_in addr;
     unsigned int addr_len = sizeof(addr);
     if (getsockname(sockDesc, (sockaddr *) &addr, (socklen_t *) &addr_len) < 0) {
-        // throw SocketException("Fetch of local address failed (getsockname())", true);
+        perror("Fetch of local address failed (getsockname())");
     }
     return inet_ntoa(addr.sin_addr);
 }
@@ -69,7 +51,7 @@ unsigned short Socket::getLocalPort() {
     sockaddr_in addr;
     unsigned int addr_len = sizeof(addr);
     if (getsockname(sockDesc, (sockaddr *) &addr, (socklen_t *) &addr_len) < 0) {
-        // throw SocketException("Fetch of local port failed (getsockname())", true);
+        perror("Fetch of local port failed (getsockname())");
     }
     return ntohs(addr.sin_port);
 }
@@ -83,7 +65,7 @@ void Socket::setLocalPort(unsigned short localPort) {
     localAddr.sin_port = htons(localPort);
     
     if (bind(sockDesc, (sockaddr *) &localAddr, sizeof(sockaddr_in)) < 0) {
-        // throw SocketException("Set of local port failed (bind())", true);
+        perror("Set of local port failed (bind())");
     }
 }
 
@@ -93,11 +75,9 @@ void Socket::setLocalAddressAndPort(const string &localAddress, unsigned short l
     fillAddr(localAddress, localPort, localAddr);
     
     if (bind(sockDesc, (sockaddr *) &localAddr, sizeof(sockaddr_in)) < 0) {
-        // throw SocketException("Set of local address and port failed (bind())", true);
+        perror("Set of local address and port failed (bind())");
     }
 }
-
-void Socket::cleanUp() {}
 
 unsigned short Socket::resolveService(const string &service, const string &protocol) {
     struct servent *serv;        /* Structure containing service information */
@@ -121,20 +101,20 @@ void CommunicatingSocket::connect(const string &foreignAddress, unsigned short f
     
     // Try to connect to the given port
     if (::connect(sockDesc, (sockaddr *) &destAddr, sizeof(destAddr)) < 0) {
-        // throw SocketException("Connect failed (connect())", true);
+        perror("Connect failed (connect())");
     }
 }
 
 void CommunicatingSocket::send(const void *buffer, int bufferLen) {
     if (::send(sockDesc, (raw_type *) buffer, bufferLen, 0) < 0) {
-        // throw SocketException("Send failed (send())", true);
+        perror("Send failed (send())");
     }
 }
 
 int CommunicatingSocket::recv(void *buffer, int bufferLen)  {
     int rtn;
     if ((rtn = ::recv(sockDesc, (raw_type *) buffer, bufferLen, 0)) < 0) {
-        // throw SocketException("Received failed (recv())", true);
+        perror("Received failed (recv())");
     }
     return rtn;
 }
@@ -144,7 +124,7 @@ string CommunicatingSocket::getForeignAddress() {
     unsigned int addr_len = sizeof(addr);
     
     if (getpeername(sockDesc, (sockaddr *) &addr,(socklen_t *) &addr_len) < 0) {
-        // throw SocketException("Fetch of foreign address failed (getpeername())", true);
+        perror("Fetch of foreign address failed (getpeername())");
     }
     return inet_ntoa(addr.sin_addr);
 }
@@ -154,7 +134,7 @@ unsigned short CommunicatingSocket::getForeignPort() {
     unsigned int addr_len = sizeof(addr);
     
     if (getpeername(sockDesc, (sockaddr *) &addr, (socklen_t *) &addr_len) < 0) {
-        // throw SocketException("Fetch of foreign port failed (getpeername())", true);
+        perror("Fetch of foreign port failed (getpeername())");
     }
     return ntohs(addr.sin_port);
 }
@@ -162,7 +142,7 @@ unsigned short CommunicatingSocket::getForeignPort() {
 // TCPSocket Code
 
 TCPSocket::TCPSocket() : CommunicatingSocket(SOCK_STREAM, IPPROTO_TCP) {}
-    
+
 TCPSocket::TCPSocket(const string &foreignAddress, unsigned short foreignPort) : CommunicatingSocket(SOCK_STREAM, IPPROTO_TCP) {
     connect(foreignAddress, foreignPort);
 }
@@ -172,132 +152,27 @@ TCPSocket::TCPSocket(int newConnSD) : CommunicatingSocket(newConnSD) {
 
 // TCPServerSocket Code
 
-TCPServerSocket::TCPServerSocket(unsigned short localPort, int queueLen) 
-: Socket(SOCK_STREAM, IPPROTO_TCP) {
-setLocalPort(localPort);
-setListen(queueLen);
+TCPServerSocket::TCPServerSocket(unsigned short localPort, int queueLen) : Socket(SOCK_STREAM, IPPROTO_TCP) {
+    setLocalPort(localPort);
+    setListen(queueLen);
 }
 
-TCPServerSocket::TCPServerSocket(const string &localAddress, 
-unsigned short localPort, int queueLen) 
-: Socket(SOCK_STREAM, IPPROTO_TCP) {
-setLocalAddressAndPort(localAddress, localPort);
-setListen(queueLen);
+TCPServerSocket::TCPServerSocket(const string &localAddress, unsigned short localPort, int queueLen) : Socket(SOCK_STREAM, IPPROTO_TCP) {
+    setLocalAddressAndPort(localAddress, localPort);
+    setListen(queueLen);
 }
 
 TCPSocket *TCPServerSocket::accept() {
     int newConnSD;
     if ((newConnSD = ::accept(sockDesc, NULL, 0)) < 0) {
-    // throw SocketException("Accept failed (accept())", true);
+        perror("Accept failed (accept())");
     }
-
+    
     return new TCPSocket(newConnSD);
 }
 
 void TCPServerSocket::setListen(int queueLen) {
-if (listen(sockDesc, queueLen) < 0) {
-// throw SocketException("Set listening socket failed (listen())", true);
-}
-}
-
-// UDPSocket Code
-
-UDPSocket::UDPSocket() : CommunicatingSocket(SOCK_DGRAM,
-IPPROTO_UDP) {
-setBroadcast();
-}
-
-UDPSocket::UDPSocket(unsigned short localPort)  : 
-CommunicatingSocket(SOCK_DGRAM, IPPROTO_UDP) {
-setLocalPort(localPort);
-setBroadcast();
-}
-
-UDPSocket::UDPSocket(const string &localAddress, unsigned short localPort) 
-: CommunicatingSocket(SOCK_DGRAM, IPPROTO_UDP) {
-setLocalAddressAndPort(localAddress, localPort);
-setBroadcast();
-}
-
-void UDPSocket::setBroadcast() {
-// If this fails, we'll hear about it when we try to send.  This will allow 
-// system that cannot broadcast to continue if they don't plan to broadcast
-int broadcastPermission = 1;
-setsockopt(sockDesc, SOL_SOCKET, SO_BROADCAST, 
-(raw_type *) &broadcastPermission, sizeof(broadcastPermission));
-}
-
-void UDPSocket::disconnect() {
-sockaddr_in nullAddr;
-memset(&nullAddr, 0, sizeof(nullAddr));
-nullAddr.sin_family = AF_UNSPEC;
-
-// Try to disconnect
-if (::connect(sockDesc, (sockaddr *) &nullAddr, sizeof(nullAddr)) < 0) {
-#ifdef WIN32
-if (errno != WSAEAFNOSUPPORT) {
-#else
-if (errno != EAFNOSUPPORT) {
-#endif
-// throw SocketException("Disconnect failed (connect())", true);
-}
-}
-}
-
-void UDPSocket::sendTo(const void *buffer, int bufferLen, 
-const string &foreignAddress, unsigned short foreignPort) 
-{
-sockaddr_in destAddr;
-fillAddr(foreignAddress, foreignPort, destAddr);
-
-// Write out the whole buffer as a single message.
-if (sendto(sockDesc, (raw_type *) buffer, bufferLen, 0,
-(sockaddr *) &destAddr, sizeof(destAddr)) != bufferLen) {
-// throw SocketException("Send failed (sendto())", true);
-}
-}
-
-int UDPSocket::recvFrom(void *buffer, int bufferLen, string &sourceAddress, unsigned short &sourcePort) {
-sockaddr_in clntAddr;
-socklen_t addrLen = sizeof(clntAddr);
-int rtn;
-if ((rtn = recvfrom(sockDesc, (raw_type *) buffer, bufferLen, 0, 
-(sockaddr *) &clntAddr, (socklen_t *) &addrLen)) < 0) {
-// throw SocketException("Receive failed (recvfrom())", true);
-}
-sourceAddress = inet_ntoa(clntAddr.sin_addr);
-sourcePort = ntohs(clntAddr.sin_port);
-
-return rtn;
-}
-
-void UDPSocket::setMulticastTTL(unsigned char multicastTTL) {
-if (setsockopt(sockDesc, IPPROTO_IP, IP_MULTICAST_TTL, 
-(raw_type *) &multicastTTL, sizeof(multicastTTL)) < 0) {
-// throw SocketException("Multicast TTL set failed (setsockopt())", true);
-}
-}
-
-void UDPSocket::joinGroup(const string &multicastGroup) {
-struct ip_mreq multicastRequest;
-
-multicastRequest.imr_multiaddr.s_addr = inet_addr(multicastGroup.c_str());
-multicastRequest.imr_interface.s_addr = htonl(INADDR_ANY);
-if (setsockopt(sockDesc, IPPROTO_IP, IP_ADD_MEMBERSHIP, 
-(raw_type *) &multicastRequest, 
-sizeof(multicastRequest)) < 0) {
-// throw SocketException("Multicast group join failed (setsockopt())", true);
-}
-}
-
-void UDPSocket::leaveGroup(const string &multicastGroup) {
-struct ip_mreq multicastRequest;
-
-multicastRequest.imr_multiaddr.s_addr = inet_addr(multicastGroup.c_str());
-multicastRequest.imr_interface.s_addr = htonl(INADDR_ANY);
-if (setsockopt(sockDesc, IPPROTO_IP, IP_DROP_MEMBERSHIP, 
-(raw_type *) &multicastRequest, 
-sizeof(multicastRequest)) < 0) {
-// throw SocketException("Multicast group leave failed (setsockopt())", true);
-}
+    if (listen(sockDesc, queueLen) < 0) {
+        perror("Set listening socket failed (listen())");
+    }
 }
