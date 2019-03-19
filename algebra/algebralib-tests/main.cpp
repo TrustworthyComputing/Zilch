141c47c65dd344e3ffce9e200a8e5fd9ca69492b
Dimitris Mouris <jimouris@udel.edu>
2019-03-19 13:54:17 -0400

    Remove tests

--- algebra/algebralib-tests/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-
-int main(int argc, char *argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-	size_t res = RUN_ALL_TESTS();
-    return res;
-}
--- tinyram/gadgetlib/gadgetlib/tests/gadgetlib2_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-#define IRR_DEGREE 63
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
