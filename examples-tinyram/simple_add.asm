9aac1c7378bec9b6d54e966c9f57fe1277a3116a
Dimitris Mouris <jimouris@udel.edu>
2019-04-12 17:18:11 -0400

    Add ZMIPS parser

--- examples-tinyram/simple_add.asm
+++ examples-tinyram/simple_add.asm
@@ -1,10 +1,10 @@
-move $t2, $t2, 13
-addi $t2, $t2, 7
-move $t1, $t1, 12
-sub $t1, $t1, 1
-sub $t1, $t1, 1
-sll $t1, $t1, 1
-lw $t5, 4($sp)
-lw $t5, 512
-addu $t3, $t1, $t2
-answer $t3, $t3, $t3
+MOV r2 r2 13
+ADD r2 r2 7
+MOV r1 r1 12
+SUB r1 r1 1
+SUB r1 r1 1
+JMP r0 r0 __L1__
+__L1__
+SHL r1 r1 1
+ADD r3 r1 r2
+ANSWER r3 r3 r3
---
+++ tinyram/stark-tinyram/src/zMipsParser.hpp
@@ -0,0 +1,19 @@
+#ifndef ZMIPS_PARSER_HPP
+#define ZMIPS_PARSER_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <regex>
+
+#include "TinyRAM/TinyRAMInstance.hpp"
+
+using namespace std;
+
+string remove_extension(const string&);
+string fromZMips(string instr, const string&, const string&, const string&);
+inline bool isInteger(const string&);
+string mapMipsRegister(string&);
+string parseZmips(const string);
+
+#endif // ZMIPS_PARSER_HPP
